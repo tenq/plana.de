c7393168342c13d88abffcc16a58e892a7f66db3
Viktor Lavnejev <v.lavnejev@gmail.com>
2022-01-24 17:25:42 +0200

    bugfix

--- brainson_theme/assets/src/scripts/smooth-scroll.js
+++ brainson_theme/assets/src/scripts/smooth-scroll.js
@@ -1,16 +1,18 @@
-// const links = document.querySelectorAll(".smooth-scroll");
-//
-// for (const link of links) {
-//     link.addEventListener("click", clickHandler);
-// }
-//
-// function clickHandler(e) {
-//     e.preventDefault();
-//     const href = this.getAttribute("href");
-//     const offsetTop = document.querySelector(href).offsetTop;
-//
-//     scroll({
-//         top: offsetTop,
-//         behavior: "smooth"
-//     });
-// }
+const links = document.getElementsByTagName("a");
+
+for (const link of links) {
+    if (link.href.includes('#')) {
+        link.addEventListener("click", clickHandler);
+    }
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
